567566060961880e14caa916a3008786f1493d9b
thangcao <thangcao@kms-technology.com>
2016-08-17 21:27:01 +0700

    Update ticketControllers

--- routes/controllers/ticketController.js
+++ routes/controllers/ticketController.js
@@ -47,41 +47,69 @@ var ticketController = function() {
 
     };
     var updateWorkerForTicket = function(req, res) {
-        var responsible = {
-            "id": req.body.idWorker,
-            "username": req.body.nameOfWorker,
-            "phoneNumber": req.body.phoneNumber,
-            "profileImage": {
-                "imageUrl": req.body.imageOfWorker,
-                "width": 60,
-                "height": 60
-            }
-        }
-        Ticket.findOne({
-            _id: req.params.ticket_id
-        }, function(err, ticket) {
-            if (!err) {
-                ticket.responsible = responsible
-                ticket.status = "Inservice"
-                if (!ticket) {
-
+        try {
+            var responsible = {
+                "id": req.body.idWorker,
+                "username": req.body.nameOfWorker,
+                "phoneNumber": req.body.phoneNumber,
+                "profileImage": {
+                    "imageUrl": req.body.imageOfWorker,
+                    "width": 60,
+                    "height": 60
                 }
-                ticket.save(function(err) {
-                    if (err)
-                        res.send(err);
-                    res.json({
-                        status: 200,
-                        message: 'Ticket has been updated',
-                        data: ticket
-                    });
-                });
             }
-        });
+            Ticket.findOne({
+                _id: req.params.ticket_id
+            }, function(err, ticket) {
+                if (!err) {
+                    ticket.responsible = responsible
+                    ticket.status = "InService"
+                    ticket.save(function(err) {
+                        if (err)
+                            res.send(err);
+                        res.json({
+                            status: 200,
+                            message: 'Ticket has been updated',
+                            data: ticket
+                        });
+                    });
+                }
+            });
+        } catch (err) {
+            pingoLogger.log(err);
+            res.json("Error");
+        }
+
+    };
+    var updateStatusOfTicket = function(req, res) {
+        try {
+            var status = "Done"
+            Ticket.findOne({
+                _id: req.params.ticket_id
+            }, function(err, ticket) {
+                if (!err) {
+                    ticket.status = status
+                    ticket.save(function(err) {
+                        if (err)
+                            res.send(err);
+                        res.json({
+                            status: 200,
+                            message: 'Ticket has been updated status',
+                            data: ticket
+                        });
+                    });
+                }
+            });
+        } catch (err) {
+            pingoLogger.log(err);
+            res.json("Error");
+        }
     }
     return {
         createNewTicket: createNewTicket,
         showTicket: showTicket,
-        updateWorkerForTicket: updateWorkerForTicket
+        updateWorkerForTicket: updateWorkerForTicket,
+        updateStatusOfTicket: updateStatusOfTicket
     }
 
 }();
